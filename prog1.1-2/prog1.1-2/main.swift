6cac19437db9cf36157f1d549a2c982af321c7ab
Ethan Brake <ebrake@oucs1425.otago.ac.nz>
2018-07-11 11:35:22 +1200

    Replace words with emojis

---
+++ prog1.1-2/prog1.1-2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  prog1.1-2
+//
+//  Created by Ethan Brake on 7/11/18.
+//  Copyright © 2018 Ethan Brake. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
